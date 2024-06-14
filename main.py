78ae2b524cf65c8cfd108ec2901f3e9e315b1958
YoungMan5 <166776800+YoungMan5@users.noreply.github.com>
2024-06-14 12:40:24 +0900

    add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import random  # randomライブラリを追加
 
 app = FastAPI()
 
@@ -12,3 +13,38 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/present")
+async def new_naming(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
