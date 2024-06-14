b9465a25407ef142c33a80c997a97af06d901a69
YoungMan5 <166776800+YoungMan5@users.noreply.github.com>
2024-06-14 12:25:25 +0900

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "all righi"}
+    return {"message": "all right"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
